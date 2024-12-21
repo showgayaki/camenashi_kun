26c70e1f5634e16d89414c03f5d0ded7dae34238
showgayaki <knk.lee.kj@gmail.com>
2024-12-22 00:19:00 +0900

    SSHサーバー上のファイルの日時取得方法を変更しました

--- camenashi_kun/ssh.py
+++ camenashi_kun/ssh.py
@@ -1,7 +1,9 @@
-import paramiko
-import datetime
+import datetime as dt
+from datetime import datetime
 from pathlib import Path
 from logging import getLogger
+
+import paramiko
 
 
 logger = getLogger(__name__)
@@ -19,7 +21,7 @@ class Ssh:
         if 'port' not in self.config:
             self.config['port'] = 22
 
-        logger.info(f'ssh config => {self.config}')
+        logger.info(f'SSH config: {self.config}')
 
     def sftp_upload(self, local: str, server: str) -> bool:
         logger.info('Starting SFTP upload.')
@@ -43,15 +45,16 @@ class Ssh:
         finally:
             client.close()
 
-    def remove_old_files(self, dir: str, threshold_storage_days: int) -> None:
+    def remove_old_files(self, target_dir: str, threshold_storage_days: int) -> None:
         logger.info('Starting remove old files on SSH server.')
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.WarningPolicy())
         client.load_system_host_keys()
 
         # 経過日数計算用
-        today = datetime.datetime.today()
-        threshold_timedelta = datetime.timedelta(days=threshold_storage_days)
+        today = datetime.today()
+        threshold_timedelta = dt.timedelta(days=threshold_storage_days)
+        logger.info(f'Removing files older than {(today - dt.timedelta(days=threshold_storage_days)).date()}')
 
         try:
             client.connect(
@@ -62,32 +65,34 @@ class Ssh:
             )
             sftp_connection = client.open_sftp()
 
-            # dir変数から絶対パスを取得
-            stdin, stdout, stderr = client.exec_command(f'cd {dir}; pwd')
-            dir = [line.strip() for line in stdout][0]
             # dir内のファイルで、拡張子が.mp4のファイルのみリストにして返す
-            file_list = [file for file in sftp_connection.listdir(dir) if Path(file).suffix == '.mp4']
+            file_list = [file for file in sftp_connection.listdir(target_dir) if Path(file).suffix == '.mp4']
             file_list.sort()
 
+            sftp_connection.chdir(target_dir)
             removed_files = []
             for file_name in file_list:
-                # ファイル名から日時を取得して、経過日数を計算
-                file_date = datetime.datetime.strptime(Path(file_name).stem, '%Y%m%d-%H%M%S')
+                # ファイル更新日時を取得して、保存期間を過ぎているか検証
+                file_date = datetime.fromtimestamp(sftp_connection.stat(file_name).st_mtime)
                 difference = today - file_date
 
                 # 保存期間を過ぎたら削除
                 if difference > threshold_timedelta:
-                    file_path = Path(dir).joinpath(file_name)
+                    file_path = Path(target_dir).joinpath(file_name)
                     removed_files.append(str(file_path))
-                    client.exec_command('rm {}'.format(file_path))
+                    client.exec_command(f'rm {file_path}')
                     logger.info(f'Remove file: {file_path}')
 
             if len(removed_files) == 0:
-                logger.info('No files found to delete on the SSH server.')
+                logger.info('No files found to remove on the SSH server.')
             else:
                 logger.info(f'Removed files: {removed_files}')
         except Exception as e:
             logger.error(e)
         finally:
             client.close()
-            del client, stdin, stdout, stderr
+
+
+def test():
+    ssh = Ssh('okomesan')
+    ssh.remove_old_files('/share/Camenashi', 150)
